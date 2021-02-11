00d3eb0eb7edb69b2df664a0e4c678b83dc72e30
Syd Sisco <hello@sydsis.co>
2021-02-11 12:48:23 -0800

    implements promises!

---
+++ index2.js
@@ -0,0 +1,26 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const date = new Date(pass.risetime * 1000);
+    console.log(`Next pass at ${date} for ${Math.round(pass.duration / 60)} minutes and ${pass.duration % 60} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then(passTimes => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  })
+  
+
+// const tempFunc = (passTimes) => {
+//   for (const pass of passTimes) {
+//     const date = new Date(pass.risetime * 1000);
+//     console.log(`Next pass at ${date} for ${Math.round(pass.duration / 60)} minutes and ${pass.duration % 60} seconds!`);
+//   }
+// }
+// nextISSTimesForMyLocation()
+//   .then(tempFunc);
