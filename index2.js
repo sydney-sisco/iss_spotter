a51f4bf0b1151fdf41b5055826a8acbb9f7cc3f1
Syd Sisco <hello@sydsis.co>
2021-02-11 12:49:26 -0800

    lints

--- index2.js
+++ index2.js
@@ -13,14 +13,4 @@ nextISSTimesForMyLocation()
   })
   .catch((error) => {
     console.log("It didn't work: ", error.message);
-  })
-  
-
-// const tempFunc = (passTimes) => {
-//   for (const pass of passTimes) {
-//     const date = new Date(pass.risetime * 1000);
-//     console.log(`Next pass at ${date} for ${Math.round(pass.duration / 60)} minutes and ${pass.duration % 60} seconds!`);
-//   }
-// }
-// nextISSTimesForMyLocation()
-//   .then(tempFunc);
+  });
