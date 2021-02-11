2cf440840fa85a712c5caa20d5b0b8e74677673e
Syd Sisco <hello@sydsis.co>
2021-02-11 04:36:27 +0000

    implements fetching geo coords from freegeoip.app

--- index.js
+++ index.js
@@ -8,3 +8,14 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+// const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('64.46.11.1411', (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coords:' , data);
+// });
--- iss.js
+++ iss.js
@@ -28,4 +28,28 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coords. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // const data = JSON.parse(body);
+    // const coords = {
+    //   latitude: data.latitude,
+    //   longitude: data.longitude
+    // };
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
